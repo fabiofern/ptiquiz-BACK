a17d254429525e62075b170b082e3575288715c7
Your name <abdelkarim.bounoua@gmail.com>
2025-06-09 09:27:14 +0200

    set model user adding avatar

--- database/models/User.js
+++ database/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	avatar: { type: String, default: null },
 });
 
 module.exports = mongoose.models.users || mongoose.model("users", schema);
